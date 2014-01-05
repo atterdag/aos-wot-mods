9e36fc9bd5b83a2cc61d82db5edd9314c44d3f95
valdemar@lemche.net <valdemar@lemche.net@a7727c8c-c604-42c3-2b6a-261f1c3c3cc4>
2014-01-05 11:44:09 +0000

    Added support for the aos-wot-mods folder

--- generate-file-list.bat
+++ generate-file-list.bat
@@ -1,3 +1,5 @@
 $ECHO OFF
-DIR /S /B /A:-D /O:GN res_mods > aos-wot-mods-files.txt
-DIR /S /B /AD /O:GN res_mods > aos-wot-mods-directories.txt
+DIR /S /B /A:-D /O:GN res_mods > res_mods-files.txt
+DIR /S /B /AD /O:GN res_mods > res_mods-directories.txt
+DIR /S /B /A:-D /O:GN aos-wot-mods > aos-wot-mods-files.txt
+DIR /S /B /AD /O:GN aos-wot-mods > aos-wot-mods-directories.txt
