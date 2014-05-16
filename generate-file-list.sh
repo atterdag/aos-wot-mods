9023ac56f331a3ae2a5c565fc0dc02c0bdae3159
valdemar@lemche.net <valdemar@lemche.net@a7727c8c-c604-42c3-2b6a-261f1c3c3cc4>
2014-05-16 21:40:04 +0000

    Modified to support files names with spaces

--- generate-file-list.sh
+++ generate-file-list.sh
@@ -4,14 +4,17 @@ if [ -f delme.txt ]; then
 	rm -f delme.txt
 fi
 
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 for folder in {aos-wot-mods,res_mods,res}; do
 	for d in $($USERPROFILE/bin/find.exe $folder -type d); do
-		echo "    RMDir \$INSTDIR\\$d" >> delme.txt
+		echo "    RMDir \"\$INSTDIR\\$d\"" >> delme.txt
 	done
 	for f in $($USERPROFILE/bin/find.exe $folder -type f); do
-		echo "    Delete \$INSTDIR\\$f" >> delme.txt
+		echo "    Delete \"\$INSTDIR\\$f\"" >> delme.txt
 	done
 done
+IFS=$SAVEIFS
 
 tac delme.txt | sed 's/\//\\/g' > file_and_directory_list.txt
 
